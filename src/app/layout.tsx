c37c2ebd3386323fe931c66ff56ce17b5f83c29c
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-07-04 12:22:07 +0900

    add: header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <h2>포켓몬 도감</h2>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
