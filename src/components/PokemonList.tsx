7bf2d2e69750aa59fef7db7cf1f9937531e9bb59
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-07-05 13:13:49 +0900

    style: pokemon list

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -22,29 +22,39 @@ const PokemonList: React.FC = () => {
   }, []);
 
   return (
-    <div>
-      {pokemons.length === 0 ? (
-        <div>
-          <p>로딩 중입니다.</p>
-        </div>
-      ) : (
-        <div>
-          {pokemons.map((pokemon) => (
-            <div>
-              <Link href={`/pokemon/${pokemon.id}`}>
-                <Image
-                  src={pokemon.sprites.front_default}
-                  alt={pokemon.korean_name}
-                  width={96}
-                  height={96}
-                />
-                <p>{pokemon.korean_name}</p>
-                <p>도감번호: {pokemon.id}</p>
-              </Link>
-            </div>
-          ))}
-        </div>
-      )}
+    <div className="bg-gray-100 min-h-screen">
+      <div className="container mx-auto p-4">
+        {pokemons.length === 0 ? (
+          <div className="text-center">
+            <p>로딩 중입니다.</p>
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+            {pokemons.map((pokemon) => (
+              <div
+                key={pokemon.id}
+                className="bg-white rounded-lg shadow-md p-4"
+              >
+                <Link href={`/pokemon/${pokemon.id}`}>
+                  <div className="flex flex-col items-center">
+                    <Image
+                      src={pokemon.sprites.front_default}
+                      alt={pokemon.korean_name}
+                      width={96}
+                      height={96}
+                      className="rounded-full"
+                    />
+                    <p className="mt-2 text-lg font-semibold">
+                      {pokemon.korean_name}
+                    </p>
+                    <p className="text-gray-500">도감번호: {pokemon.id}</p>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
